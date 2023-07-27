c46f6487098aec28949e3b1366442a79dff8912d
root <root@Im-Laptop.localdomain>
2023-07-26 20:40:36 -0400

    seeds for places

--- app/seeds/places.py
+++ app/seeds/places.py
@@ -10,3 +10,31 @@ def seed_places():
         state='New York',
         activity_type='roller coster rides'
     )
+    dorne_park = Place(
+        name='Dorne Park',
+        address='123 test lane',
+        city='Jersey City',
+        state='New Jersey',
+        activity_type='water park'
+    )
+    bronx_zoo = Place(
+        name='Bronx Zoo',
+        address='123 abc lane',
+        city='Bronx',
+        state='New York',
+        activity_type='zoo'
+    )
+
+    db.session.add(five_flags)
+    db.session.add(dorne_park)
+    db.session.add(bronx_zoo)
+    db.session.commit()
+
+
+def undo_places():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.places RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM places"))
+
+    db.session.commit()
