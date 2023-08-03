25215754b3569c352711e586c294306e294b4613
root <root@Im-Laptop.localdomain>
2023-08-03 12:43:56 -0400

    POST route to add a place to place_to_visit table

--- app/api/place_to_visit.py
+++ app/api/place_to_visit.py
@@ -1,6 +1,7 @@
-from flask import Flask, Blueprint, jsonify, session
+from flask import Flask, Blueprint, jsonify, session, request
 from flask_login import login_required
 from app.models.place_to_visit import Place_To_Visit, db
+from app.forms.add_place_to_visit_form import Add_place_to_visit
 
 place_to_visit_routes = Blueprint('placesToVisit', __name__)
 
@@ -16,4 +17,27 @@ def placesToVisit():
     likedPlaces = Place_To_Visit.query.filter(Place_To_Visit.user_id == user_idd).all()
     print(likedPlaces, '--------liked')
 
-    return {"tset": [likedPlace.to_dict_place_to_visit() for likedPlace in likedPlaces]}
+    return {"place_to_visit": [likedPlace.to_dict_place_to_visit() for likedPlace in likedPlaces]}
+
+# add place to the places to visit
+@place_to_visit_routes.route('/<int:id>', methods=['POST'])
+@login_required
+def addPlace(id):
+    """
+    add place to list
+    """
+    form = Add_place_to_visit()
+    print(form,'-------form')
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        newPlace = Place_To_Visit(
+            place_id = id,
+            user_id = int(session['_user_id']),
+            status = form.data['status']
+        )
+        print(newPlace, '------------new')
+        db.session.add(newPlace)
+        db.session.commit()
+
+        return newPlace.to_dict_place_to_visit()
+    return ({'Error': 'Please Try Again'}), 404
