eb973e35a3710831fcddae2d2186de181de1c4c8
root <root@Im-Laptop.localdomain>
2023-08-05 12:31:47 -0400

    route for creating new cart

--- app/api/cart_order_routes.py
+++ app/api/cart_order_routes.py
@@ -1,18 +1,49 @@
 from flask import Flask, Blueprint, session, request
 from flask_login import login_required
-from app.models.cart_order import Cart_Order
+from app.models.cart_order import Cart_Order, db
 
 cartOrder = Blueprint('cart', __name__)
 
 # get a cart based on user id
-@cartOrder.route('/<int:userId>')
+@cartOrder.route('/user/<int:userId>')
+@login_required
 def getCartForUser(userId):
     """
     get cart base on user id
     """
-    print(userId, type(userId), '----userId')
+    # print(userId, type(userId), '----userId')
 
     userCarts = Cart_Order.query.filter(Cart_Order.user_id == userId).all()
-    print(userCarts, '-----cart')
+    print(userCarts, dir(userCarts[0]), '-----cart')
 
     return { 'Order History': [userCart.to_dict_cart_order() for userCart in userCarts]}
+
+# post req in cart
+@cartOrder.route('/new', methods=['POST'])
+@login_required
+def newCart():
+    """_summary_ new cart
+    - check if cart exists
+    - check if user exists
+    """
+    user = session
+
+    # query based on user Id and payment
+    checkCart = Cart_Order.query.filter(Cart_Order.user_id == int(user['_user_id']), Cart_Order.payment == False).all()
+
+    if not checkCart:
+        cartCreated = Cart_Order(
+            user_id = int(user['_user_id']),
+            total = 10,
+            payment = False
+        )
+
+        db.session.add(cartCreated)
+        db.session.commit()
+
+        # print(user, user['_user_id'], type(user['_user_id']), '----------userCart')
+        print(checkCart, cartCreated, '------------cart')
+
+        return cartCreated.to_dict_cart_order()
+    return ({'Error': 'Something went wrong'}), 404
+
