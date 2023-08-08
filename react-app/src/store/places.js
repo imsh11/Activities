20f3cc7f7667ceddd060746c5518cf50a60ad613
root <root@Im-Laptop.localdomain>
2023-08-08 07:38:52 -0400

    store updated for details page

--- react-app/src/store/places.js
+++ react-app/src/store/places.js
@@ -1,4 +1,5 @@
 const GET_ALL_PLACES = 'activities/getAllPlaces'
+const GET_PLACE_DETAIL = 'activities/getDetailByPlaceId'
 
 
 //regular
@@ -9,10 +10,17 @@ const allPlaces = (places) => {
     }
 }
 
+const detailByPlaceId = (detail) => {
+    return{
+        type: GET_PLACE_DETAIL,
+        payload: detail
+    }
+}
+
 
 //thunk
 export const getAllPlaces = () => async (dispatch) => {
-    console.log('IN THUNK')
+    // console.log('IN THUNK')
     const placeResponse = await fetch('/api/place/allPlaces')
 
     // console.log(placeResponse, '----resp fetch')
@@ -22,6 +30,23 @@ export const getAllPlaces = () => async (dispatch) => {
         console.log(data, '---data')
 
         dispatch(allPlaces(data))
+        return data
+    }
+}
+
+export const getDetailByPlaceId = (id) => async(dispatch) => {
+
+    const response = await fetch(`/api/place/${id}`)
+
+    console.log(response, '------------resp Detail')
+
+    if(response.ok){
+        const data = await response.json()
+
+        console.log(data, '---------------data')
+
+        dispatch(detailByPlaceId(data))
+
         return data
     }
 }
@@ -41,6 +66,17 @@ const placeReducer = (state = initialState, action) => {
             action.payload.Places.forEach( ele => newState[ele.id] = ele)
 
             // console.log(newState, '----newState')
+            return newState
+        }
+        case GET_PLACE_DETAIL: {
+            const newState = {}
+
+            console.log(action.payload, '--------payload')
+
+            newState['Place'] = action.payload.Place
+            newState['Reviews'] = action.payload.Reviews
+            console.log(newState, '----newState')
+
             return newState
         }
         default:
