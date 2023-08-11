4ecf442ced7d23b9de7d7827b5910de9877163e2
root <root@Im-Laptop.localdomain>
2023-08-10 21:46:37 -0400

    modal for updating quantity with validations

---
+++ react-app/src/components/UpdateItemQuantity/updateQuantity.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { getUserCart } from "../../store/cart";
+
+
+
+const UpdateQuantity = ({}) => {
+
+    const [quantity, setQuantity] = useState('')
+    const [validation, setValidation] = useState('')
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const user = useSelector(state => state.session.user)
+    const item = useSelector(state => state)
+    console.log(user, item)
+
+    // useEffect(() => {
+    //     dispatch(getUserCart(user.id))
+    // }, [dispatch])
+
+    return (
+        <>
+            Test
+        </>
+    )
+}
+
+
+export default UpdateQuantity
