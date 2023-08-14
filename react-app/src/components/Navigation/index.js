2affc88cebba870e7af148be9eb12384ab0a2363
root <root@Im-Laptop.localdomain>
2023-08-14 16:00:01 -0400

    css added to landing page component

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -9,12 +9,33 @@ function Navigation({ isLoaded }){
 
 	return (
 		<div className='nav-container'>
-			<div>
-				<NavLink exact to="/">Home</NavLink>
+			<div className='nav-homeBtn'>
+			<span className="main-page-tooltip">{"Home"}</span>
+				<NavLink exact to="/">
+				<i class="fa-solid fa-house fa-lg" style={{color: "#f2780c"}}></i>
+				</NavLink>
 			</div>
 			{isLoaded && (
-				<div>
-					<ProfileButton user={sessionUser} />
+				<div className='nav-right'>
+					<div className='nav-listBtn'>
+							<span className="main-page-tooltip">{"Place List"}</span>
+						<button >
+							<NavLink exact to='/user/placeList'>
+							<i class="fa-solid fa-list fa-lg" style={{color: "#f2780c"}}></i>
+							</NavLink>
+						</button>
+					</div>
+					<div className='nav-cartBtn'>
+					<span className="main-page-tooltip">{"Cart"}</span>
+						<button className=''>
+							<NavLink exact to='/user/cart'>
+								<i class="fa-solid fa-cart-shopping fa-lg" style={{color: "#f2780c"}}></i>
+							</NavLink>
+						</button>
+					</div>
+					<div className='nav-profile'>
+						<ProfileButton user={sessionUser} />
+					</div>
 				</div>
 			)}
 		</div>
