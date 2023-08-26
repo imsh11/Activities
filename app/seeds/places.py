cd8d6283ee172c168bd7fc86572dcf1fa88b68e4
root <root@Im-Laptop.localdomain>
2023-08-26 19:26:29 -0400

    seed data for places added

--- app/seeds/places.py
+++ app/seeds/places.py
@@ -93,6 +93,81 @@ def seed_places():
         price= 50,
         activity_type='aqurium'
     )
+    place4 = Place(
+        name= 'The Museum of Modern Art',
+        address= '11 W 53rd St',
+        city= 'New York',
+        state= 'NY',
+        zipcode= '10019',
+        img1= 'https://media.istockphoto.com/id/486985302/photo/moma-entrance-in-new-york.jpg?s=612x612&w=0&k=20&c=BEEzWMVe8rYA_4kvdiA3_pdLXXDPYwqOIOW8vIlQsn8=',
+        img2= 'https://image.newyork.co.uk/wp-content/uploads/2014/09/MoMA-Museum-of-Modern-Art-in-New-York-Empty-Museum.jpg.webp',
+        img3= 'https://unframed.lacma.org/sites/default/files/field/image/20210325_LACMA_Modern_015.jpg',
+        img4= 'https://img1.10bestmedia.com/Images/Photos/224574/p-moma_55_660x440_201404240658.jpg',
+        img5= 'https://news.artnet.com/app/news-upload/2017/06/view05_axon-1024x768.jpg',
+        product= 'one day ticket',
+        price= 40,
+        activity_type='museum'
+    )
+    place5 = Place(
+        name= 'Brooklyn Museum',
+        address= '200 Eastern Pkwy',
+        city= 'Brooklyn',
+        state= 'NY',
+        zipcode= '11238',
+        img1= 'https://www.insidehook.com/wp-content/uploads/2018/04/GettyImages-475931048-e1522595273603-1.jpg?fit=1178%2C800',
+        img2= 'https://d1lfxha3ugu3d4.cloudfront.net/images/opencollection/exhibitions/size3/DIG_E_2012_Connecting_Cultures_01_PS4.jpg',
+        img3= 'https://cdn.goppion.com/uploads/images/Projects/legacy/_projectIntroImage/brooklyn-museum-001.jpg',
+        img4= 'https://d1lfxha3ugu3d4.cloudfront.net/images/opencollection/exhibitions/size2/DIG_E_2014_Double_Take_African_Innovations_06_PS4.jpg',
+        img5= 'https://www.brooklynmuseum.org/images/luce/map/floor_plan5_floor.jpg',
+        product= 'one day ticket',
+        price= 30,
+        activity_type='museum'
+    )
+    place6 = Place(
+        name= 'The Metropolitan Museum of Art',
+        address= '1000 5th Ave',
+        city= 'New York',
+        state= 'NY',
+        zipcode= '10028',
+        img1= 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg/1200px-Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg',
+        img2= 'https://www.metmuseum.org/-/media/images/art/collection-landing-page/mahaney.jpg?h=560&iar=0&mw=840&w=840&sc_lang=en&hash=E4472034C1704AF23E246B8B92F9F7DE',
+        img3= 'https://1.bp.blogspot.com/-fL2HkPCJo84/XllGzXlVOBI/AAAAAAABEYo/-FcEOd4mIvs8PTORQg8dFsor0lYd1FvOQCLcBGAsYHQ/s1600/The%2BMetroplitan%2BMuseum%2Bof%2BArt%2BMet%2BNew%2BYork%2BCity%2Bart%2Bgallery%2BElle%2BField.jpg',
+        img4= 'https://voyages.topexpos.fr/wp-content/uploads/2018/06/collection-art-greco-romain-met-museum-new-york.jpg',
+        img5= 'https://static01.nyt.com/images/2016/12/19/well/met_map_thumbnail/met_map_thumbnail-superJumbo-v2.png',
+        product= 'one day ticket',
+        price= 65,
+        activity_type='museum'
+    )
+    place7 = Place(
+        name= 'Adventure Aquarium',
+        address= '1 Riverside Dr',
+        city= 'Camden',
+        state= 'NJ',
+        zipcode= '08103',
+        img1= 'https://visitsouthjersey.com/wp-content/uploads/2022/06/front-9-1024x577.jpeg',
+        img2= 'https://townsquare.media/site/385/files/2020/07/AP_050519013622.jpg',
+        img3= 'https://pbs.twimg.com/media/FZRfm7KXkAApGCb.jpg',
+        img4= 'https://images.squarespace-cdn.com/content/v1/608892bc6c86d971c15161ee/1619809570234-VZ70FN5PD0LEQ058JN7P/image-asset.jpeg',
+        img5= 'https://1.bp.blogspot.com/-3WThCuhbDjQ/WlqWEM25XmI/AAAAAAAACmA/pMMJ90lx97EvMLWqO1QmTCvMNG2TkOXrACLcBGAs/s1600/AdvendureAquariumMap05.jpg',
+        product= 'one day ticket',
+        price= 55,
+        activity_type='aquarium'
+    )
+    place8 = Place(
+        name= 'SeaQuest Woodbridge',
+        address= '101 Woodbridge Center Dr',
+        city= 'Woodbridge Township',
+        state= 'NJ',
+        zipcode= '07095',
+        img1= 'https://patch.com/img/cdn20/users/22870800/20191119/022357/styles/patch_image/public/img-1575___19135134779.jpg',
+        img2= 'https://fastly.4sqi.net/img/general/600x600/550735611_yqjXKIpvNHyifMnvvo7ddpejMa7xQfvfC6J22iic0EU.jpg',
+        img3= 'https://fastly.4sqi.net/img/general/600x600/10393037_x1e7IUUrLOiYZ6UUNDUpGT9C28y1DnnE-0W7zL7n4Gc.jpg',
+        img4= 'https://patch.com/img/cdn20/users/22870800/20190423/123422/styles/raw/public/admiring-animal-aquarium-baby-beautiful-below-20190423121552-31870400.jpg',
+        img5= 'https://visitseaquest.com/woodbridge/wp-content/uploads/sites/11/2022/07/home-blog-img.webp',
+        product= 'one day ticket',
+        price= 35,
+        activity_type='aquarium'
+    )
 
     db.session.add(six_flags)
     db.session.add(dorne_park)
@@ -100,6 +175,11 @@ def seed_places():
     db.session.add(place1)
     db.session.add(place2)
     db.session.add(place3)
+    db.session.add(place4)
+    db.session.add(place5)
+    db.session.add(place6)
+    db.session.add(place7)
+    db.session.add(place8)
     db.session.commit()
 
 
