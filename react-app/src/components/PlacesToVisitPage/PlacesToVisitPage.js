c785c12adb4ac53f8afc30c28c7f8ad9206538be
root <root@Im-Laptop.localdomain>
2023-08-30 20:34:16 -0400

    content added for place

--- react-app/src/components/PlacesToVisitPage/PlacesToVisitPage.js
+++ react-app/src/components/PlacesToVisitPage/PlacesToVisitPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom/cjs/react-router-dom";
 import { getAllPlaces } from "../../store/places";
@@ -13,11 +13,15 @@ const UserPlaceList = () => {
     const dispatch = useDispatch()
     const history = useHistory()
 
+    const [isLoaded, setIsLoaded] = useState(false)
+
     const user = useSelector(state => state.session.user)
     const placeList = useSelector(state => state.placesList)
-    console.log(user, placeList,  '---------user')
+    const places = useSelector(state => state.places)
+    console.log(user, placeList, places, '---------user')
 
     useEffect(() => {
+        dispatch(getAllPlaces()).then(() => setIsLoaded(true))
         dispatch(getPlaceToVisit())
     }, [dispatch, user])
 
@@ -29,35 +33,57 @@ const UserPlaceList = () => {
 
     return(
         <>
-        <div>
-            <h2>Place I Wish to Visit</h2>
-        </div>
-        <div>
-            <h4>Hello {user.firstname}!</h4>
-        </div>
-            {Object.values(placeList).length ?
-        <div>
-            {placeList && Object.values(placeList).map(
-                place => (
-                    <div key={place.id} className="placeList">
-                        <div>
-                            Place: {place.place_id}
-                        </div>
-                        <div>
-                            My Status: {place.status}
-                        </div>
-                        <div>
-                            <UpdateStatusPlace place={place} />
-                        </div>
-                        <div>
-                            <DeletePlace id={place.id} />
-                        </div>
-                    </div>
+        {isLoaded && (
+        <div className="place-list-main">
+            <div>
+                <h2>Place I Wish to Visit</h2>
+            </div>
+            <div>
+                <h4>Hello {user.firstname}!</h4>
+            </div>
+                {Object.values(placeList).length ?
+            <div>
+                {placeList && Object.values(placeList).map(
+                    place => (
+                        <div key={place.id} className="placeList">
+                            <div className="placeList-img">
+                                <img className="place-List-img" src={places[place.place_id].img1}
+                                alt={places[place.place_id].name} />
+                            </div>
+                            <div className="placeList-detail">
+                                <a className="place-list-name" href={`/place/${place.place_id}`}>
+                                    {places[place.place_id].name}
+                                </a>
+                                <div className="place-list-address">
+                                    Location: {places[place.place_id].address},&nbsp;
+                                    {places[place.place_id].state},&nbsp;{places[place.place_id].zipcode}
+                                </div>
+                                <div className="place-list-price">
+                                    ${places[place.place_id].price}
+                                </div>
+                            </div>
+                            <div className="placeList-update">
+                                <div className="status">
+                                    Status: {place.status}
+                                </div>
+                                <div className="place-list-btn">
+                                    <div className="place-updateBtn">
+                                        <UpdateStatusPlace place={place} />
+                                    </div>
+                                    <div className="place-delBtn">
+                                        <DeletePlace id={place.id} />
+                                    </div>
+                                </div>
 
-                ))}
-        </div>:
-        <div>You Can View Places <NavLink exact to="/">Here </NavLink>
-            And Add Places You Plan on Visiting.</div>}
+                            </div>
+
+                        </div>
+                    ))}
+            </div>:
+            <div>You Can View Places <NavLink exact to="/">Here </NavLink>
+                And Add Places You Plan on Visiting.</div>}
+        </div>
+        )}
         </>
     )
 }
