0606a4ade5e3d3e6c213f1567532af3e3b56bac4
root <root@Im-Laptop.localdomain>
2023-09-05 18:54:48 -0400

    remove place modal css done

--- react-app/src/components/PlaceDeleteFromPlaceList/PlaceDeleteModal.js
+++ react-app/src/components/PlaceDeleteFromPlaceList/PlaceDeleteModal.js
@@ -33,15 +33,28 @@ export default function DeletePlace ({id}) {
             {modal && (
                     <div id="modal">
                         <div id="modal-background" onClick={toggleModal}></div>
-                        <div id="modal-content" className="modal-container">
-                            <h3>Confirm</h3>
-                            <p className="deletetext">Are you sure you want to remove this Place?</p>
-                            <button className="modal-button remove-button button1" onClick={handleRemove}>
-                                Remove
-                            </button>
-                            <button className="modal-button cancle-button button2"onClick={toggleModal}>
-                                Cancel
-                            </button>
+                        <div id="modal-content-del" className="modal-container">
+                            <div className="modal-content-inner">
+                                <div className="cartDel-first">
+                                    <i className="fa-solid fa-triangle-exclamation fa-2xl" style={{color: "#fa0511"}}></i>
+                                </div>
+                                <div className="cartDel-Second placeList-main">
+                                    <div className="del-main-title">Removing from List</div>
+                                    <div className="deletetext">Are you sure you want to remove this Place?</div>
+                                </div>
+                            </div>
+                            <div className="cart-delBtn">
+                                <div>
+                                    <button className="modal-button keep-button-del button1"onClick={toggleModal}>
+                                        Cancel
+                                    </button>
+                                </div>
+                                <div>
+                                    <button className="modal-button delete-button button1" onClick={handleRemove}>
+                                        Remove
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 )}
