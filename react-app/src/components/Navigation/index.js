f82655298e8899584c9e54209d5fffd6a0af1532
root <root@Im-Laptop.localdomain>
2023-09-09 20:02:16 -0400

    tooltip added on landing page

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import { Tooltip } from '../Tooltip/Tooltip';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
@@ -11,26 +12,32 @@ function Navigation({ isLoaded }){
 		<div className='nav-container'>
 			<div className='nav-homeBtn'>
 			{/* <span className="main-page-tooltip">{"Home"}</span> */}
+			<Tooltip text={'Home'}>
 				<NavLink exact to="/">
 				<i class="fa-solid fa-house fa-lg" style={{color: "#f2780c"}}></i>
 				</NavLink>
+			</Tooltip>
 			</div>
 			{isLoaded && (
 				<div className='nav-right'>
-					<div className='nav-listBtn'>
-							{/* <span className="main-page-tooltip">{"Place List"}</span> */}
-						{/* <button > */}
-							<NavLink exact to='/user/placeList'>
-							<i class="fa-solid fa-list fa-lg" style={{color: "#f2780c"}}></i>
-							</NavLink>
-						{/* </button> */}
-					</div>
+						<div className='nav-listBtn'>
+							<Tooltip text={"Place List"}>
+								{/* <span className="main-page-tooltip">{"Place List"}</span> */}
+							{/* <button > */}
+								<NavLink exact to='/user/placeList'>
+								<i class="fa-solid fa-list fa-lg" style={{color: "#f2780c"}}></i>
+								</NavLink>
+							{/* </button> */}
+							</Tooltip>
+						</div>
 					<div className='nav-cartBtn'>
 					{/* <span className="main-page-tooltip">{"Cart"}</span> */}
 						{/* <button className=''> */}
+						<Tooltip text={'Cart'}>
 							<NavLink exact to='/user/cart'>
 								<i class="fa-solid fa-cart-shopping fa-lg" style={{color: "#f2780c"}}></i>
 							</NavLink>
+						</Tooltip>
 						{/* </button> */}
 					</div>
 					<div className='nav-profile'>
