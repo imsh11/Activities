b41946f6d821b0cd8409a111e3d483a05ce51378
root <root@Im-Laptop.localdomain>
2023-09-26 20:58:19 -0400

    image slider functionality added

--- react-app/src/components/imageSlider/ImageSlider.js
+++ react-app/src/components/imageSlider/ImageSlider.js
@@ -25,10 +25,73 @@ const ImageSlider= ({slides}) => {
         backgroundImage: `url(${slides[currentIndex].url})`
         // backgroundImage: `url("${variable}")`
     }
+
+    const leftArrow = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        left: "32px",
+        fontSize: '40px',
+        color: '#fff',
+        zIndex: 1,
+        cursor: 'pointer'
+    };
+
+    const rightArrow = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        right: "32px",
+        fontSize: '40px',
+        color: '#fff',
+        zIndex: 1,
+        cursor: 'pointer'
+    };
+
+    const dotContainer = {
+        display: 'flex',
+        justifyContent: 'center',
+    };
+
+    const dotStyles = {
+        margin: '2px 3px',
+        cursor: 'pointer',
+        fontSize: '15px'
+    }
+
+    const goToPrevious = () => {
+        const isFirstSlide = currentIndex === 0
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex)
+    };
+
+    const goToNext = () => {
+        const isLastSlide = currentIndex === slides.length - 1
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex)
+    };
+
+    const goToIndex = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    }
+
     return(
         <div style={sliderStyles}>
-            <div><i class="fa-solid fa-chevron-left fa-lg"></i></div>
+            <div style={leftArrow} onClick={goToPrevious}>
+                <i className="fa-solid fa-chevron-left fa-lg"></i>
+            </div>
+            <div style={rightArrow} onClick={goToNext}>
+                <i className="fa-solid fa-chevron-right fa-lg"></i>
+            </div>
             <div style={slideStyles}></div>
+            <div style={dotContainer}>
+                {slides.map((slide, slideIndex) =>(
+                    <div key={slideIndex} style={dotStyles}
+                    onClick={() => goToIndex(slideIndex)}>
+                        <i class="fa-solid fa-circle fa-sm"></i>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
