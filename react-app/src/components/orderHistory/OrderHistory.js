8f114a9b47cb6caee5d7389b4910f6f5c768a5d2
root <root@Im-Laptop.localdomain>
2023-10-06 19:47:03 -0400

    place name added to order history

--- react-app/src/components/orderHistory/OrderHistory.js
+++ react-app/src/components/orderHistory/OrderHistory.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getOrderHistoryByUserId } from "../../store/order";
+import { getAllPlaces } from "../../store/places";
+import ItemQuantityForm from "../ItemQuantityForm/ItemQuantityForm";
 
 const OrderHistoryByUserId = () => {
 
@@ -9,33 +11,40 @@ const OrderHistoryByUserId = () => {
     const[isLoaded, setIsLoaded] = useState(false)
 
     const userOrders = useSelector(state => Object.values(state.orderHistory))
+    const user = useSelector(state => state.session.user)
+    const places = useSelector(state => state.places)
     console.log(userOrders, '-------order compo')
 
     useEffect(() => {
+        dispatch(getAllPlaces())
         dispatch(getOrderHistoryByUserId()).then(() => setIsLoaded(true))
-    }, [dispatch])
+    }, [dispatch, user])
 
     return(
         <>
+        {isLoaded &&(
             <div>
-                TEsting
-            </div>
-            <div>
-                {userOrders.map( ele =>(
-                    <div key={ele.id} style={{border:'solid'}}>
-                        {ele.items.map(item => (
-                        <div key={item.id}>
-                            <div>
-                                Place: {item.place_id}
+                <div>
+                    TEsting
+                </div>
+                <div>
+                    {userOrders.map( ele =>(
+                        <div key={ele.id} style={{border:'solid'}}>
+                            {ele.items.map(item => (
+                            <div key={item.id}>
+                                <div>
+                                    Place: {places[item.place_id].name}
+                                </div>
+                                <div>
+                                    Qty: {item.quantity}
+                                </div>
                             </div>
-                            <div>
-                                Qty: {item.quantity}
-                            </div>
+                        ))}
                         </div>
                     ))}
-                    </div>
-                ))}
+                </div>
             </div>
+        )}
         </>
     )
 }
