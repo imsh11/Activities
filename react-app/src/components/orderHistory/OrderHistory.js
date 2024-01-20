e21d8d63016883b2322f9f120373e61b5ec88afa
root <root@Im-Laptop.localdomain>
2024-01-19 19:53:58 -0500

    keys for reivewed places added to array

--- react-app/src/components/orderHistory/OrderHistory.js
+++ react-app/src/components/orderHistory/OrderHistory.js
@@ -16,7 +16,9 @@ const OrderHistoryByUserId = () => {
     const userOrders = useSelector(state => Object.values(state.orderHistory))
     const user = useSelector(state => state.session.user)
     const places = useSelector(state => state.places)
-    console.log(userOrders, '-------order compo')
+    const review = useSelector(state => Object.values(state.reviews)) // user reviews
+    // const review = useSelector(state => Object.keys(state.reviews)) // user reviews
+    console.log(userOrders, review, '-------order compo')
 
     useEffect(() => {
         dispatch(getAllPlaces())
@@ -24,6 +26,12 @@ const OrderHistoryByUserId = () => {
     }, [dispatch, user])
 
     let total;
+
+    // places user have reviewed already
+    let revPlaces = []
+    review.forEach( (obj) => revPlaces.push(obj.place_id))
+
+    console.log(revPlaces, '--------revPlaces')
 
     return(
         <>
@@ -55,6 +63,17 @@ const OrderHistoryByUserId = () => {
                                     </div>
                                 </div>
                                 <div className="add-review">
+                                    {revPlaces.includes(item.place_id)?
+                                    <div>
+                                        <button>
+                                            
+                                        </button>
+                                    </div>
+                                    :
+                                    <div>
+
+                                    </div>
+                                    }
                                     <CreateReview id={item.place_id} />
                                 </div>
                                 
