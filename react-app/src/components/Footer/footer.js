9556ac2368c0e65011c1b0eb03d4619265f799c2
root <root@Im-Laptop.localdomain>
2024-03-06 20:49:26 -0500

    footer js and css created

---
+++ react-app/src/components/Footer/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div>
+            <div>
+                Test
+            </div>
+            <div>
+                Test2
+            </div>
+        </div>
+    )
+}
+
+export default Footer
